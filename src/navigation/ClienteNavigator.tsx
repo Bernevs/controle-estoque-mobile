dbcae24b65475e977bc31338f71f4af4c5572b3f
Bernevs <berneves2003@gmail.com>
2025-07-03 13:32:47 -0300

    feat: :sparkles: cria modal de edicao de cliente

--- src/navigation/ClienteNavigator.tsx
+++ src/navigation/ClienteNavigator.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ClienteHome from "../screens/cliente/ClienteHome";
 import ClienteMenu from "../screens/cliente/ClienteMenu";
 import { createStackNavigator } from "@react-navigation/stack";
 import IconButton from "../components/IconButton";
+import EditarCliente from "../screens/cliente/EditarCliente";
 
 export type ClienteStackParamList = {
   ClienteHome: undefined;
@@ -12,32 +13,43 @@ export type ClienteStackParamList = {
 const Stack = createStackNavigator<ClienteStackParamList>();
 
 export default function ClienteNavigator() {
+  const [editarModal, setEditarModal] = useState<boolean>(false);
+  const [cliente, setCliente] = useState<number>(0);
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="ClienteHome"
-        component={ClienteHome}
-        options={{ headerShown: false }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name={"ClienteMenu"}
-        component={ClienteMenu}
-        options={({ route }) => ({
-          title: route.params.nome,
-          headerRight: () => (
-            <>
-              <IconButton
-                iconName="pencil"
-                onPress={() => console.log("editar")}
-              />
-              <IconButton
-                iconName="cash-outline"
-                onPress={() => console.log("dinheiro")}
-              />
-            </>
-          ),
-        })}
-      ></Stack.Screen>
-    </Stack.Navigator>
+    <>
+      <EditarCliente
+        id={cliente}
+        modalVisible={editarModal}
+        onClose={() => setEditarModal(false)}
+      ></EditarCliente>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="ClienteHome"
+          component={ClienteHome}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name={"ClienteMenu"}
+          component={ClienteMenu}
+          options={({ route }) => ({
+            title: route.params.nome,
+            headerRight: () => (
+              <>
+                <IconButton
+                  iconName="pencil"
+                  onPress={() => {
+                    setCliente(route.params.clienteId), setEditarModal(true);
+                  }}
+                />
+                <IconButton
+                  iconName="cash-outline"
+                  onPress={() => console.log("dinheiro")}
+                />
+              </>
+            ),
+          })}
+        ></Stack.Screen>
+      </Stack.Navigator>
+    </>
   );
 }
