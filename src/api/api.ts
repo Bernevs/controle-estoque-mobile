35ea206792011406e57364ffa280a194e4e5465c
Bernevs <berneves2003@gmail.com>
2025-08-12 14:51:42 -0300

    fix: :bug: ajusta endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const endpoint = "http://192.168.100.247:3000";
+export const endpoint = "https://naturagleici-api.vercel.app";
 const api = axios.create({ baseURL: endpoint });
 
 export default api;
